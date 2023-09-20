7991c9e787cd2b8f714838683bbfc00ae26f33ea
Gurumurthi2005 <144354710+Gurumurthi2005@users.noreply.github.com>
2023-09-20 18:23:20 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,15 +1,31 @@
-def is_leap_year(year):
-    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-        return True
-    else:
-        return False
+class Player:
+    def play(self):
+        print("The player is playing cricket.")
 
-try:
-    year = int(input("Enter a year: "))
-    
-    if is_leap_year(year):
-        print(f"{year} is a leap year.")
-    else:
-        print(f"{year} is not a leap year.")
-except ValueError:
-    print("Invalid input. Please enter a valid year (integer).")
+class Batsman(Player):
+    def play(self):
+        print("The batsman is batting.")
+
+class Bowler(Player):
+    def play(self):
+        print("The bowler is bowling.")
+
+# Main program
+while True:
+    try:
+        choice = int(input("Enter 1 for Batsman, 2 for Bowler, or 0 to exit: "))
+        if choice == 0:
+            break
+        elif choice == 1:
+            player = Batsman()
+        elif choice == 2:
+            player = Bowler()
+        else:
+            print("Invalid choice. Please enter 1 for Batsman, 2 for Bowler, or 0 to exit.")
+            continue
+
+        player.play()
+    except ValueError:
+        print("Invalid input. Please enter a valid choice.")
+
+print("Exiting the program.")
